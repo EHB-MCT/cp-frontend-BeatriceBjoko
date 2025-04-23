aaa19924b530c4d41cb57ae402230a382cc137ac
BeatriceBjoko <beatrice.bjoko@student.ehb.be>
2025-04-23 11:04:16 +0200

    Stap 2: react router installeren en opzetten

---
+++ courseproject/src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import MakingOf from "./pages/MakingOf";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/projects" element={<Projects />} />
+				<Route path="/making-of/:projectId" element={<MakingOf />} />
+			</Routes>
+			<Footer />
+		</>
+	);
+}
+
+export default App;
